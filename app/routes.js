6cceb457373a8f4ddac5756ea718d245fc0ca005
Emma James <ejames@companieshouse.gov.uk>
2022-09-23 09:54:57 +0100

    Routing for document delivery added and example basket pages created

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,26 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+
+
+// Dispatch radio buttons routing
+
+router.post('/docs-delivery-option', function (req, res) {
+
+    if(req.session.data['choose-dispatch-option'] == "digital"){
+
+      res.redirect('basket-one-digital-item')
+    }
+    if(req.session.data['choose-dispatch-option'] == "express"){
+
+      res.redirect('delivery-details')
+    }
+    else if(req.session.data['choose-dispatch-option'] == "standard"){
+
+      res.redirect('delivery-details')
+    }
+})
+
+
 module.exports = router
