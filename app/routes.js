4bccdf9e05f51d3d3413e43fed05b4b3066b07e6
Emma James <ejames@companieshouse.gov.uk>
2022-09-30 11:32:42 +0100

    Scenario routing added
    Summary of scenario:
    
    Add a digital copy of:
    04 Jul 2018
    Confirmation statement made on 27 June 2018 with no updates
    
    Add a digital copy of:
    29 Jun 2017
    Group of companies' accounts made up to 28 January 2017
    
    Add a physical copy of:
    04 Nov 2016	Statement of capital on 4 November 2016 GBP 6,750,000.00
    
    Remove the statement of capital document from the order
    
    Continue to payment

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const app = express()
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
@@ -32,19 +33,46 @@ router.post('/select-multiple-documents', function(req, res) {
 
 router.post('/docs-delivery-option', function (req, res) {
 
+
     if(req.session.data['choose-dispatch-option'] == "digital"){
 
+      if(app.set('paperOption')=="digital"){
+
+        res.redirect('basket/basket-two-items')
+
+      }
+      else{
+
+      app.set('paperOption','digital')
       res.redirect('basket/basket-one-digital-item')
+
+      }
     }
     else if(req.session.data['choose-dispatch-option'] == "paper"){
 
-      res.redirect('delivery-details')
-    }
-    /* else if(req.session.data['choose-dispatch-option'] == "standard"){
 
-      res.redirect('delivery-details')
-    } */
+      if(req.session.data['choose-paper-option'] == "standard"){
+          app.set('paperOption','standard')
+          res.redirect('delivery-details')
+      }
+      else if(req.session.data['choose-paper-option'] == "express"){
+          app.set('paperOption','express')
+          res.redirect('delivery-details')
+      }      
+    }
+
 })
 
+router.post('/delivery-details', function (req, res) {
+
+    if(app.settings.paperOption == 'standard')
+    {
+      res.redirect('basket/basket-three-items')
+    }
+    else if(app.settings.paperOption == 'express')
+    {
+      res.redirect('basket/basket-three-items')
+    }
+})
 
 module.exports = router
